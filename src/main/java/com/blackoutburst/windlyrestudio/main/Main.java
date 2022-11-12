f1f83de73019df0f0bb800175eacced084e87815
Blackoutburst <blackoutburst@hotmail.com>
2022-11-12 20:37:44 +0100

    Removed unused imports

--- src/main/java/com/blackoutburst/windlyrestudio/main/Main.java
+++ src/main/java/com/blackoutburst/windlyrestudio/main/Main.java
@@ -3,12 +3,7 @@ package com.blackoutburst.windlyrestudio.main;
 import com.blackoutburst.windlyrestudio.core.Core;
 import com.blackoutburst.windlyrestudio.core.LyrePlayer;
 import com.blackoutburst.windlyrestudio.core.audio.AudioPlayer;
-import com.blackoutburst.windlyrestudio.core.gui.NoteButton;
 import com.blackoutburst.windlyrestudio.core.gui.render.Display;
-import com.blackoutburst.windlyrestudio.core.gui.render.ShaderProgram;
-import com.blackoutburst.windlyrestudio.core.gui.render.Texture;
-import com.blackoutburst.windlyrestudio.utils.maths.Vector2f;
-import org.lwjgl.glfw.GLFW;
 
 public class Main {
 
